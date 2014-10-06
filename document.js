91dc00eb434ff8d549f5f76f2d0b446f748835c3
Chet Harrison <chet.harrison@gmail.com>
2014-10-05 22:07:16 -0700

    breaking m/r into nested vars for readability

--- document.js
+++ document.js
@@ -122,14 +122,13 @@ var fs = require('fs'),
 
 		//-----------------------------------------
 
-		var asts = [syntaxTree];
+		var asts = [syntaxTree],
 		
-		var namespaceClass = asts.
-		filter(function (ast) {
-			return ast.type === 'Program';
-		}).
-		flatMap(function (ast) {
-			return ast.body.
+			astBody = asts.
+				filter(function (ast) { return ast.type === 'Program'; }).
+				flatMap(function (ast) { return ast.body; }),
+			
+			namespaceAndClass = astBody.
 				filter(function (bodyNode) {
 					return bodyNode.type === 'ExpressionStatement' &&
 						bodyNode.expression.type === 'AssignmentExpression' &&
@@ -139,93 +138,95 @@ var fs = require('fs'),
 						bodyNode.expression.left.property &&
 						bodyNode.expression.left.property.type === 'Identifier' &&
 						bodyNode.expression.right.type !== 'FunctionExpression';
-				}).
+				}),
+				
+			namespaces = namespaceAndClass.
 				map(function (bodyNode) {
 					return {
-						namespace: bodyNode.expression.left.object.name,
-						class: bodyNode.expression.left.property.name
+						namespace: {name: bodyNode.expression.left.object.name}
 					};
+				}),
+				
+			classes = namespaceAndClass.
+				map(function (bodyNode) {
+					return {
+						class: {name: bodyNode.expression.left.property.name}
+					};
+				}),
+				
+			expressionAssignments = astBody.
+				filter(function (body) {
+					return body.type === 'ExpressionStatement';
+				}).
+				map(function (body) {
+					return body.expression;
+				}).
+				filter(function (expression) {
+					return expression.type === 'AssignmentExpression';
+				}),
+				
+			functions = expressionAssignments.
+				map(function (expression) {
+					return expression.right;
+				}).
+				filter(function (right) {
+					return right.type === 'FunctionExpression';
+				}).
+				flatMap(function (right) {
+					return right.params;
+				}),
+				
+			methods = expressionAssignments.
+				map(function (expression) {
+					return expression.right;
+				}).
+				filter(function (right) {
+					return right.type === 'CallExpression';
+				}).
+				flatMap(function (right) {
+					return right.arguments;
+				}).
+				filter(function (args) {
+					return args.type === 'ObjectExpression';
+				}).
+				flatMap(function (args) {
+					return args.properties;
+				}).
+				filter(function (properties) {
+					return properties.type === 'Property' &&
+						properties.value &&
+						properties.value.params &&
+						properties.key.type === 'Identifier';
+				}).
+				map(function (properties) {
+					var obj = {},
+						key = properties.key.name === 'constructor' ? 'constructor' : 'method';
+						
+					obj[key] = {
+						name: properties.key.name,
+						tags: [
+							// single values need to be in an array because mergeAll expects an array of 
+							// arrays so this `[ '@api private', [ '@param foo' ] ]` bombs and 
+							// `[ ['@api private'], [ '@param foo'] ]` works.
+							properties.key.name.indexOf('_') === 0 ? ['@api private'] : ['@api public'],
+							properties.value.params.
+							filter(function (param) {
+								return param.type === 'Identifier';
+							}).
+							map(function (param) {
+								return '@param {<type>} ' + param.name + ' - ';
+							})
+						].mergeAll()  // here's that mergeAll
+					};
+					
+					return obj;
 				});
-			});
+				
+			console.log('--------------');
+			console.log([namespaces, classes, methods, functions].mergeAll());
+			console.log('--------------');
+			
 		
-		console.log(namespaceClass);
-		
-		
-
-		var allMethods = asts.
-		filter(function (ast) {
-			return ast.type === 'Program';
-		}).
-		flatMap(function (ast) {
-			return ast.body.
-			filter(function (body) {
-				return body.type === 'ExpressionStatement';
-			}).
-			map(function (body) {
-				return body.expression;
-			}).
-			filter(function (expression) {
-				return expression.type === 'AssignmentExpression';
-			}).
-			map(function (expression) {
-				return expression.right;
-			}).
-			filter(function (right) {
-				return right.type === 'CallExpression';
-			}).
-			flatMap(function (right) {
-				return right.arguments;
-			}).
-			filter(function (args) {
-				return args.type === 'ObjectExpression';
-			}).
-			flatMap(function (args) {
-				return args.properties;
-			}).
-			filter(function (properties) {
-				return properties.type === 'Property' &&
-					properties.value &&
-					properties.value.params &&
-					properties.key.type === 'Identifier';
-			}).
-			map(function (properties) {
-				return {
-					type: properties.key.name === 'constructor' ? 'constructor' : 'method',
-					name: properties.key.name,
-					tags: [
-						// single values need to be in an array because mergeAll expects an array of 
-						// arrays so this `[ '@api private', [ '@param foo' ] ]` bombs and 
-						// `[ ['@api private'], [ '@param foo'] ]` works.
-						properties.key.name.indexOf('_') === 0 ? ['@api private'] : ['@api public'],
-						properties.value.params.
-						filter(function (param) {
-							return param.type === 'Identifier';
-						}).
-						map(function (param) {
-							return '@param {<type>} ' + param.name + ' - ';
-						})
-					].mergeAll()  // here's that mergeAll
-				};
-			});
-		});
-
-		// console.log(allMethods);
-		
-		// console.log([["@api private"],["@param {<type>} triggerDef - "]].mergeAll());
-
-		// var namedConstructors = allMethods
-		// .filter(function(method) { return method.name === 'constructor'; });
-
-		// console.log('Named Constructor');
-		// console.log(namedConstructors);
-
-		// var methods = allMethods
-		// .filter(function(method) { 
-		// 	return method.name !== 'constructor';
-		// });
-
-		// console.log('Methods');
-		// console.log(methods);
 
 
 

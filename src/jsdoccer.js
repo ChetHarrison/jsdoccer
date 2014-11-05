03f6dbf2111d7074dacd3565063f9fed7ecb2eb5
Chet Harrison <chet.harrison@gmail.com>
2014-11-05 12:00:05 -0800

    major refactor to separate grunt from node core

--- src/jsdoccer.js
+++ src/jsdoccer.js
@@ -2,7 +2,7 @@
 
 // Dependencies
 //-----------------------------------------
-var fs 					= require('fs'),
+var fs 					= require('fs-extra'),
 	path 				= require('path'),
 	esprima 			= require('esprima'),
 	_ 					= require('lodash'),
@@ -10,7 +10,27 @@ var fs 					= require('fs'),
 	// delegate declarations
 	_astGenerator 		= Object.create( require('./lib/ast-generator.js') ),
 	_astToDocJson 		= Object.create( require('./lib/ast-to-doc-json.js') ),
-	_docJsonToDocYaml 	= Object.create( require('./lib/doc-json-to-doc-yaml.js') );
+	_docJsonToDocYaml 	= Object.create( require('./lib/doc-json-to-doc-yaml.js') ),
+	_prepareYaml		= Object.create( require('./lib/prepare-yaml.js') ),
+	_generateDocs		= Object.create( require('./lib/generate-docs.js') ),
+	
+	// file path configuration the "setup" directory will be copied
+	// to the project root.
+	_root = '../../',
+	_config = {
+		syntaxMatchers: _root + 'jsdoccer/syntax-matchers.js',
+		setUpSrc: './setup',
+		setUpDest: _root + 'jsdoccer/',
+		yamlTemplates: _root + 'jsdoccer/templates/yaml/',
+		handlebarsTemplate: _root + 'jsdoccer/templates/yaml/',
+		jsdocTemplates: _root + 'jsdoccer/templates/jsdoc/',
+		ast: _root + 'jsdoccer/generated-files/ast/',
+		docJson: _root + 'jsdoccer/generated-files/doc-json/',
+		json: _root + 'jsdoccer/generated-files/json/',
+		yamlStubbed: _root + 'jsdoccer/generated-files/yaml/stubbed/',
+		yamlDocumented: _root + 'jsdoccer/generated-files/ymal/documented/',
+		htmlDocumentation: _root + 'jsdoccer/documentation/'
+	};
 
 
 // API
@@ -21,59 +41,106 @@ module.exports = {
 	init: function init(options) {
 		options = options || {};
 		this.config = options.config;
+		
+		// check to see if we are set up
+		try {
+			var fd = fs.openSync(_config.syntaxMatchers, 'r');
+			fs.closeSync(fd);
+		}
+		catch (err) {
+			console.log('Current Working Dir: ' + process.cwd());
+			console.log('No "' + _config.syntaxMatchers + '" setting up defaults.');
+			fs.copySync(_config.setUpSrc, _config.setUpDest);
+			console.log(err);
+		}
+		
 		_astToDocJson.init({
-			syntaxMatchers: options.config.syntaxMatchers.src
+			syntaxMatchers: _config.syntaxMatchers
 		});
+		
 		_docJsonToDocYaml.init({
-			templates: options.config.yaml.templates
+			templates: _config.yamlTemplates
+		});
+		
+		_prepareYaml.init({
+			grunt: options.grunt
+		});
+		
+		_generateDocs.init({
+			files: {
+				src: _config.docJson,
+				dest: _config.htmlDocumentation
+			},
+			handlebarsTemplate: _config.handlebarsTemplate
 		});
 	},
 	
-	
-	saveFile: function(data, filename, filepath, extention) {
-		var dest = path.join(filepath + path.basename(filename, '.js') + extention);
+	// utilities
+	saveFile: function(data, file, filepath, extention) {
+		var dest = path.join(filepath + path.basename(file, '.js') + extention);
 		
 		fs.writeFileSync(dest, data);
 	},
 	
 	
-	generateStubbedDocYamlFile: function (filename) {
-		var syntaxTree, lookup, json, docYaml;
-	
-		// gard: filter files listed in config
-		if (_.contains(this.config.filesToFilter, filename)) { return; }
-		// generate AST
-		syntaxTree = _astGenerator.createSyntaxTree(filename);
-		if (this.config.ast.save && this.config.ast.save === true) {
-			this.saveFile(JSON.stringify(syntaxTree, null, 2), filename, this.config.ast.dest, '.ast');
-		}
-		// filter AST and generate syntax target JSON
-		_astToDocJson.setFilename(filename);
-		json = _astToDocJson.parse(syntaxTree);
-		if (this.config.json.save && this.config.json.save === true) {
-			this.saveFile(JSON.stringify(json, null, 4), filename, this.config.json.dest, '.json');
-		}
-		// generate document YAML
-		docYaml = _docJsonToDocYaml.convert(json);
-		this.saveFile(docYaml, filename, this.config.yaml.dest, '.yaml');
-	},
-	
-	
-	
-	generateStubbedDocYamlFiles: function () {
+	mapFiles: function (dir, process) {
 		var self 			= this,
-			filesToDocument = fs.readdirSync(this.config.js.src);
+			filesToDocument = fs.readdirSync(dir);
 			
 		if (filesToDocument.length > 0) {
-			filesToDocument.forEach(function(file) { self.generateStubbedDocYamlFile(file); });
+			filesToDocument.forEach(function(file) { process(file); });
 		}
 		// bad usage
 		else {
-			console.warn('No js targets found to document in ' + this.config.js.src);
+			console.warn('No js targets found to document in ' + dir);
 			process.exit(1);
 		}
 	},
 	
+	// control
+	generateStubbedDocYamlFile: function (file) {
+		var syntaxTree, lookup, json, docYaml;
+	
+		// gard: filter files listed in config
+		if (_.contains(this.config.filesToFilter, file)) { return; }
+		// generate AST
+		syntaxTree = _astGenerator.createSyntaxTree(file);
+		this.saveFile(JSON.stringify(syntaxTree, null, 2), file, _config.ast, '.ast');
+		
+		// filter AST and generate syntax target JSON
+		_astToDocJson.setFilename(file);
+		json = _astToDocJson.parse(syntaxTree);
+		this.saveFile(JSON.stringify(json, null, 2), file, _config.json, '.json');
+	
+		// generate document YAML
+		docYaml = _docJsonToDocYaml.convert(json);
+		this.saveFile(docYaml, file, _config.yamlStubbed, '.yaml');
+	},
+	
+	
+	generateStubbedDocYamlFiles: function (files) {
+		this.mapFiles(files, this.generateStubbedDocYamlFile);
+	},
+	
+	
+	prepareYaml: function (file) {
+		_prepareYaml.prepare(file);
+	},
+	
+	
+	prepareYamls: function(files) {
+		this.mapFiles(files, this.prepareYaml);
+	},
+	
+	
+	generateDoc: function (file) {
+		_generateDocs.generate(file);
+	},
+	
+	
+	generateDocs: function(files) {
+		this.mapFiles(files, this.generateDoc);
+	},
 	
 	
 	lintDocumentJson: function() {
---
+++ src/lib/generate-docs.js
@@ -0,0 +1,82 @@
+'use strict';
+/**
+ * 1. run grunt api
+ * 2. read the json for each jsdoc
+ * 3. compile master json and write it to the api folder
+ * 4. build the html files
+ */
+ 
+var bluebird = require('bluebird'),
+	_ = require('lodash'),
+	path = require('path'),
+	fs = bluebird.promisifyAll(require('fs')),
+	Handlebars = require('handlebars'),
+	taskName = 'jsDoccer:html';
+	
+
+module.exports = {
+	
+	init: function (options) {
+		this.options = options || {};
+		this.files = options.files;
+		this.handlebarsTemplate = options.handlebarsTemplate;
+	},
+
+	buildJsonFile: function () {
+		var jsonFiles = _.map(this.files.src, function (filename) {
+			var content = fs.readFileSync(filename).toString();
+			return JSON.parse(content);
+		});
+
+		var apiJson = {};
+		apiJson.classes = [];
+		apiJson.functions = {};
+		apiJson.properties = {};
+
+		_.each(jsonFiles, function (jsonFile) {
+			if (_.has(jsonFile, 'class')) {
+				apiJson.classes.push(jsonFile);
+			} else {
+				if (_.has(jsonFile, 'functions')) {
+					_.extend(apiJson.functions, jsonFile.functions);
+				}
+				if (_.has(jsonFile, 'properties')) {
+					_.extend(apiJson.properties, jsonFile.properties);
+				}
+			}
+		});
+
+		if (!fs.existsSync(path.resolve(this.files.dest))) {
+			fs.mkdirSync(path.resolve(this.files.dest));
+		}
+
+		var apiFilePath = path.resolve(this.files.dest, 'api.json');
+		fs.writeFileSync(apiFilePath, JSON.stringify(apiJson, null, '  '));
+	},
+	
+	buildHtmlFiles: function () {
+		var json = JSON.parse(fs.readFileSync(path.resolve(this.files.dest, 'api.json')));
+		var classTpl = Handlebars.compile(fs.readFileSync(this.handlebarsTemplate).toString());
+
+		_.each(json.classes, function (klass) {
+			var data = {
+				marionette: json,
+				klass: klass
+			};
+
+			var classHtml = classTpl(data);
+			var classPath = path.resolve(this.files.dest, klass.name + '.html');
+
+			console.log('writing ' + klass.name + ' api file.');
+			console.log(classPath);
+
+			fs.writeFileSync(classPath, classHtml);
+
+		}, this);
+	},
+	
+	generate: function () {
+		this.buildJsonFile();
+		this.buildHtmlFiles();
+	}
+};

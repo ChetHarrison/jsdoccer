8ea62302e83b9113587610197c7fe199a258c4bf
Sam Saccone <samccone@gmail.com>
2015-02-20 23:18:43 -0500

    Update doc bin
    * No longer wrap in custom try catch
      * Let node take care of formatting the stack trace, it is much more
      * reliable
    
    * Allow users to pass in input glob via the CLI
    * Allow users to pass in output dir via the CLI

--- bin/doc.js
+++ bin/doc.js
@@ -5,20 +5,29 @@
 var jsDoccer = require('../src/jsdoccer.js'),
   configLoader = require('../src/util/config-loader.js'),
   nodeStackTracer = require('../src/util/node-stack-tracer.js'),
-
+  minimist = require('minimist'),
+  glob = require('glob'),
+  path = require('path'),
   args = Array.prototype.slice.call(process.argv, 2),
-  filesDocumented = 0; // a count of the documented files
+  filesDocumented = 0, // a count of the documented files
+  src, output;
+
+var parsedArgs = minimist(args);
 
 jsDoccer.init(configLoader('.jsdoccerrc'));
 
-try {
-  // if no args were passed in pull from the config file
-  args = !!args.length ? args : jsDoccer.config.documentedYaml.src;
-  filesDocumented = jsDoccer.doc(args);
-  console.log('Done: Generated ' + filesDocumented + ' file documents at');
-  console.log(jsDoccer.config.dest + 'docs'); // to do pull this from config
-} catch (err) {
-  console.warn('Unable to generate documentation. Did you remember to copy your YAML from the "' + jsDoccer.config.dest + 'generated/yaml/stubbed/" folder to the ".jsdoccerrc.documentedYaml.src" folder?');
-  console.warn('Useage: node doc "./path/to/target/file.yaml". Or set up your "documentedYaml.src" default targets in the ".jsdoccerrc" config file to run it with out arguments.');
-  nodeStackTracer(err);
+
+if (parsedArgs.src) {
+  src = path.join(process.cwd(), parsedArgs.src);
 }
+
+if (parsedArgs.output) {
+  output = path.join(process.cwd(), parsedArgs.output);
+}
+
+filesDocumented = jsDoccer.doc(
+  glob.sync(src || jsDoccer.config.documentedYaml.src), output
+);
+
+console.log('Done: Generated ' + filesDocumented + ' file documents at');
+
